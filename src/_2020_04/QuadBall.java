1e3ed8dfb82c4d4ef99aa00ad432d9de1a62fb21
Krabcode <jakubpodtrzitkorak@gmail.com>
2020-04-02 12:38:31 +0200

    update

--- src/_2020_04/QuadBall.java
+++ src/_2020_04/QuadBall.java
@@ -45,201 +45,61 @@ public class QuadBall extends KrabApplet {
         if (toggle("no fill")) {
             pg.noFill();
         }
-        drawQuadBall(sliderInt("diameter", 400), sliderInt("n", 10));
-        pg.camera();
-        pg.translate(sliderXYZ("moon translate").x, sliderXYZ("moon translate").y, sliderXYZ("moon translate").z);
-        pg.stroke(picker("moon stroke").clr());
-        pg.fill(picker("moon fill").clr());
-        drawQuadBall(sliderInt("moon diameter", 200), sliderInt("moon n", 5));
+        nkingBall_2(sliderInt("diameter", 400), sliderInt("n", 10));
+
         pg.endDraw();
         image(pg, 0, 0);
         rec(pg);
         gui();
     }
 
-    void drawQuadBall(float diam, int n) {
-        int divs = 1 + 2 * n;
-        float divSize = diam / divs;
-        float rad = diam / 2;
-        pg.beginShape(QUADS);
-        PVector corner = new PVector();
-        PVector v = new PVector();
-        float freq = slider("freq", 1);
-        float amp = slider("amp", 100);
-        // XY
-        float noise = 0;
-        for (int j = -1; j <= 1; j += 2) {
-            corner.set(-rad, -rad, rad * j);
-            for (int i = 0; i < divs * divs; i++) {
-                v.set(corner.x, corner.y, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x + divSize, corner.y, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x + divSize, corner.y + divSize, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x, corner.y + divSize, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                corner.add(divSize, 0, 0);
-                if (corner.x > rad - divSize / 2) {
-                    corner.x = -rad;
-                    corner.y += divSize;
-                }
-            }
-
-            //XZ
-            corner.set(-rad, rad * j, -rad);
-            for (int i = 0; i < divs * divs; i++) {
-                v.set(corner.x, corner.y, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x + divSize, corner.y, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x + divSize, corner.y, corner.z + divSize);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x, corner.y, corner.z + divSize);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                corner.add(divSize, 0, 0);
-                if (corner.x > rad - divSize / 2) {
-                    corner.x = -rad;
-                    corner.z += divSize;
-                }
-            }
-
-            // YZ
-            corner.set(rad * j, -rad, -rad);
-            for (int i = 0; i < divs * divs; i++) {
-                v.set(corner.x, corner.y, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x, corner.y + divSize, corner.z);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x, corner.y + divSize, corner.z + divSize);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                v.set(corner.x, corner.y, corner.z + divSize);
-
-                noise = (float) noiseGenerator.eval(v.x * freq, v.y * freq, v.z * freq);
-                v.setMag(rad + noise * amp);
-
-
-                pg.vertex(v.x, v.y, v.z);
-
-                corner.add(0, divSize, 0);
-                if (corner.y > rad - divSize / 2) {
-                    corner.y = -rad;
-                    corner.z += divSize;
-                }
-            }
-        }
-        pg.endShape();
-    }
-
-    void nkingBall(float r, int subDiv) {
+    void nkingBall_2(float r, int subDiv) {
         if (subDiv < 2) subDiv = 2;
-        float n = (subDiv - 1) * .5f;
         PVector[] dirs = {
-                new PVector(n, 0, 0), new PVector(-n, 0, 0),
-                new PVector(0, n, 0), new PVector(0, -n, 0),
-                new PVector(0, 0, n), new PVector(0, 0, -n)
+                new PVector(1,0,0), new PVector(-1,0,0),
+                new PVector(0,1,0), new PVector(0,-1,0),
+                new PVector(0,0,1), new PVector(0,0,-1)
         };
         PVector[] us = {
-                new PVector(0, 1, 0), new PVector(0, 0, 1),
-                new PVector(0, 0, 1), new PVector(1, 0, 0),
-                new PVector(1, 0, 0), new PVector(0, 1, 0)
+                new PVector(0,1,0), new PVector(0,0,1),
+                new PVector(0,0,1), new PVector(1,0,0),
+                new PVector(1,0,0), new PVector(0,1,0)
         };
         PVector[] vs = {
-                new PVector(0, 0, 1), new PVector(0, 1, 0),
-                new PVector(1, 0, 0), new PVector(0, 0, 1),
-                new PVector(0, 1, 0), new PVector(1, 0, 0)
+                new PVector(0,0,1), new PVector(0,1,0),
+                new PVector(1,0,0), new PVector(0,0,1),
+                new PVector(0,1,0), new PVector(1,0,0)
         };
-        beginShape(QUADS);
+        pg.beginShape(QUADS);
         PVector up = new PVector();
         PVector vp = new PVector();
         for (int i = 0; i < 6; i++) {
-            for (int u = 0; u < subDiv - 1; u++) {
-                for (int v = 0; v < subDiv - 1; v++) {
+            for (int u = 0; u < subDiv-1; u++) {
+                for (int v = 0; v < subDiv-1; v++) {
                     for (int j = 0; j < 4; j++) {
-                        float offs = (subDiv - 1) * .5f;
-                        float uPos = u - offs + (j / 2 ^ j % 2);
-                        float vPos = v - offs + j / 2;
+                        float uOffs = (j/2 ^ j%2);
+                        float vOffs = j/2;
+                        float uAngle = map(u + uOffs, 0, subDiv-1, -QUARTER_PI, QUARTER_PI);
+                        float vAngle = map(v + vOffs, 0, subDiv-1, -QUARTER_PI, QUARTER_PI);
                         up.set(us[i]);
-                        up.mult(uPos);
+                        up.mult(tan(uAngle));
                         vp.set(vs[i]);
-                        vp.mult(vPos);
+                        vp.mult(tan(vAngle));
                         PVector p = dirs[i].copy();
                         p.add(up);
                         p.add(vp);
-                        p.setMag(r);
+                        float freq = slider("frequency", 1);
+                        float noise = slider("amp", 1)* (float) noiseGenerator.eval(p.x * freq, p.y * freq, p.z * freq);
+                         freq = slider("double freq", 2);
+                        noise += slider("half amp", .5f)* (float) noiseGenerator.eval(p.x * freq, p.y * freq, p.z * freq);
+                        p.setMag(r+noise);
                         //res.normal(p.x, p.y, p.z);
-                        vertex(p.x, p.y, p.z);
+                        pg.vertex(p.x, p.y, p.z);
                     }
                 }
             }
         }
-        endShape();
+        pg.endShape();
     }
 
 }
