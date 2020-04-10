909fde0d64352a53acf8ec063d46a9ecab9d7b33
Krabcode <jakubpodtrzitkorak@gmail.com>
2020-04-10 16:25:01 +0200

    images, recursion

---
+++ src/_2020_04/ImageRecursion.java
@@ -0,0 +1,47 @@
+package _2020_04;
+
+import applet.KrabApplet;
+import processing.core.PGraphics;
+import processing.core.PImage;
+import processing.core.PVector;
+import utils.OpenSimplexNoise;
+
+public class ImageRecursion extends KrabApplet {
+    private PGraphics pg;
+    private PImage img;
+    private OpenSimplexNoise noise = new OpenSimplexNoise();
+
+    public static void main(String[] args) {
+        KrabApplet.main(String.valueOf(new Object() {
+        }.getClass().getEnclosingClass().getName()));
+    }
+
+    public void settings() {
+        size(900,674, P2D);
+    }
+
+    public void setup() {
+        pg = createGraphics(width, height, P2D);
+        img = loadImage("images/cat.png");
+        frameRecordingStarted = 1;
+
+    }
+
+    public void draw() {
+        PImage previous = pg.get();
+        pg.beginDraw();
+        pg.translate(width * .5f, height * .5f);
+        pg.imageMode(CENTER);
+        pg.image(img, 0, 0);
+        float rotateTimeRadius = slider("rotate time");
+        float rotateTimeSpeed = slider("time speed", 1);
+        pg.rotate((float) (slider("rotate amplitude") * noise.eval(rotateTimeRadius*cos(rotateTimeSpeed*t), rotateTimeRadius*sin(rotateTimeSpeed*t))));
+        float scaleTimeRadius = slider("scale time");
+        pg.scale(1 - slider("scale amplitude") * abs((float) noise.eval(scaleTimeRadius*cos(rotateTimeSpeed*t), scaleTimeRadius*sin(rotateTimeSpeed*t))));
+        pg.image(previous, 0, 0);
+        pg.endDraw();
+        image(pg, 0, 0);
+        rec(pg);
+        gui();
+    }
+}
