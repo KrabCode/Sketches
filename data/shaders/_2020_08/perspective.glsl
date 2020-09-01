0818b72a249d45d89f66f773de455a8d31904f56
Krabcode <jakubpodtrzitkorak@gmail.com>
2020-09-01 23:43:32 +0200

    improved perspective with otherworldly sights

--- data/shaders/_2020_08/perspective.glsl
+++ data/shaders/_2020_08/perspective.glsl
@@ -29,7 +29,7 @@ vec3 render(vec2 uv, float blur){
 }
 
 vec3 aaRender(vec2 uv, float blur){
-    float pixelThird = (1./resolution.x)/3.;
+    float pixelThird = (1./resolution.x)/3.0;
     vec2 aa = vec2(-pixelThird, pixelThird);
     vec3 c1 = render(uv+aa.xx, blur);
     vec3 c2 = render(uv+aa.xy, blur);
@@ -39,8 +39,8 @@ vec3 aaRender(vec2 uv, float blur){
 }
 
 vec3 perspective(vec2 uv, out float blur){
-    float distanceFromPlane = 0.5;
-    vec3 dir = vec3(uv - 0.5, distanceFromPlane);
+    float distanceFromPlane = 0.05;
+    vec3 dir = vec3(uv, distanceFromPlane);
     if (dir.y != 0.0){
         dir /= abs(dir.y);
     }
@@ -50,11 +50,27 @@ vec3 perspective(vec2 uv, out float blur){
     return dir;
 }
 
+mat2 rotate2d(float angle){
+    return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+}
+
+vec3 hash32(vec2 p)
+{
+    vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
+    p3 += dot(p3, p3.yxz+33.33);
+    return fract((p3.xxy+p3.yzz)*p3.zyx);
+}
+
+
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
+    vec2 cv = (gl_FragCoord.xy-.5*resolution) / resolution.y;
+    float angle = cos(atan(cv.y, cv.x)*1.);
+    cv *= rotate2d(angle*pi);
     float blur;
-    vec3 dir = perspective(uv, blur);
+    vec3 dir = perspective(cv, blur);
     vec3 c = aaRender(dir.xz, blur);
-    c -= pow(1.-blur, 8.);
+//    c -= pow(1.-blur, 5.);
+    c += vec3(.15*length(hash32(1000*uv.xy)));
     gl_FragColor = vec4(c, 1.);
 }
