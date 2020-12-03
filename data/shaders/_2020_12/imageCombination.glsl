6a1ace634a3d636de3cf535f77e8d2c401a5b234
Jakub Rak <jakubpodtrzitkorak@gmail.com>
2020-12-03 23:13:52 +0100

    new and improved

--- data/shaders/_2020_12/imageCombination.glsl
+++ data/shaders/_2020_12/imageCombination.glsl
@@ -4,24 +4,31 @@ uniform sampler2D img_1;
 uniform sampler2D img_2;
 uniform sampler2D img_3;
 uniform sampler2D img_4;
+uniform vec3 pos_0;
+uniform vec3 pos_1;
+uniform vec3 pos_2;
+uniform vec3 pos_3;
+uniform vec3 pos_4;
+uniform vec3 size_0;
+uniform vec3 size_1;
+uniform vec3 size_2;
+uniform vec3 size_3;
+uniform vec3 size_4;
+
 uniform vec2 resolution;
 uniform float time;
 
 void main(){
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     uv.y = 1.-uv.y;
-    vec3 i0 = texture(img_0, uv).rgb;
-    vec3 i1 = texture(img_1, uv).rgb;
-    vec3 i2 = texture(img_2, uv).rgb;
-    vec3 i3 = texture(img_3, uv).rgb;
-    vec3 i4 = texture(img_4, uv).rgb;
+    vec3 i0 = texture(img_0, size_0.xy*(uv+pos_0.xy*.0001)).rgb;
+    vec3 i1 = texture(img_1, size_1.xy*(uv+pos_1.xy*.0001)).rgb;
+    vec3 i2 = texture(img_2, size_2.xy*(uv+pos_2.xy*.0001)).rgb;
+    vec3 i3 = texture(img_3, size_3.xy*(uv+pos_3.xy*.0001)).rgb;
+    vec3 i4 = texture(img_4, size_4.xy*(uv+pos_4.xy*.0001)).rgb;
 
     vec3 col;
-
-    col = mix(
-        mix(i1, i0, smoothstep(0.7, 1.0, length(i0))),
-         i3,
-        smoothstep(1.2, 1.0, length(i2)));
+    col = i0.rgb;
 
     gl_FragColor = vec4(col, 1.);
 }
--- src/_2020_12/ImageCombination.java
+++ src/_2020_12/ImageCombination.java
@@ -3,6 +3,7 @@ package _2020_12;
 import applet.KrabApplet;
 import processing.core.PGraphics;
 import processing.core.PImage;
+import processing.core.PVector;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,26 +11,18 @@ import java.util.Map;
 public class ImageCombination extends KrabApplet {
     private PGraphics pg;
     String shader = "shaders/_2020_12/imageCombination.glsl";
+    Map<String, PImage> pathsWithImages = new HashMap<String, PImage>();
 
     public static void main(String[] args) {
         KrabApplet.main(java.lang.invoke.MethodHandles.lookup().lookupClass());
     }
 
     public void settings() {
-        size(1000, 1000, P3D);
+        size(1200, 1200, P3D);
     }
 
     public void setup() {
 
-    }
-
-    Map<String, PImage> pathsWithImages = new HashMap<String, PImage>();
-
-    PImage getImage(String path) {
-        if(!pathsWithImages.containsKey(path)) {
-            pathsWithImages.put(path, loadImage(path));
-        }
-        return pathsWithImages.get(path);
     }
 
     public void draw() {
@@ -44,11 +37,32 @@ public class ImageCombination extends KrabApplet {
     }
 
     private void updateShader(PGraphics pg) {
-        uniform(shader).set("img_0", getImage("images/sky/daniel-gregoire-LhXrNC2dn6g-unsplash.jpg").get(floor(sliderXY("0").x), floor(sliderXY("0").y), 1000, 1000));
-        uniform(shader).set("img_1", getImage("images/sky/dewang-gupta-ESEnXckWlLY-unsplash.jpg").get(floor(sliderXY("1").x), floor(sliderXY("1").y), 1000, 1000));
-        uniform(shader).set("img_2", getImage("images/sky/eberhard-grossgasteiger-J9NDmBVhN04-unsplash.jpg").get(floor(sliderXY("2").x), floor(sliderXY("2").y), 1000, 1000));
-        uniform(shader).set("img_3", getImage("images/sky/eberhard-grossgasteiger-NvesrDbsrL4-unsplash.jpg").get(floor(sliderXY("3").x), floor(sliderXY("3").y), 1000, 1000));
-        uniform(shader).set("img_4", getImage("images/sky/pero-kalimero-9BJRGlqoIUk-unsplash.jpg").get(floor(sliderXY("4").x), floor(sliderXY("4").y), 1000, 1000));
+        group("0");
+        setUniforms("0", "images/sky/daniel-gregoire-LhXrNC2dn6g-unsplash.jpg");
+        group("1");
+        setUniforms("1", "images/sky/dewang-gupta-ESEnXckWlLY-unsplash.jpg");
+        group("2");
+        setUniforms("2", "images/sky/eberhard-grossgasteiger-J9NDmBVhN04-unsplash.jpg");
+        group("3");
+        setUniforms("3", "images/sky/eberhard-grossgasteiger-NvesrDbsrL4-unsplash.jpg");
+        group("4");
+        setUniforms("4", "images/sky/pero-kalimero-9BJRGlqoIUk-unsplash.jpg");
         hotFilter(shader, pg);
+        resetGroup();
     }
+
+    void setUniforms(String group, String path) {
+        PImage img = getImage(path);
+        uniform(shader).set("img_" + group, img);
+        uniform(shader).set("pos_" + group, sliderXY("pos"));
+        uniform(shader).set("size_" + group, sliderXY("size", 1, img.width / (float) img.height));
+    }
+
+    PImage getImage(String path) {
+        if (!pathsWithImages.containsKey(path)) {
+            pathsWithImages.put(path, loadImage(path));
+        }
+        return pathsWithImages.get(path);
+    }
+
 }
