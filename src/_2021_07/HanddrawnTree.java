023e2ffdd8a5a2f6522ef47c4c3909aacd460092
Krab <krabcode@gmail.com>
2021-08-09 09:52:35 +0200

    ditched the L-Systems in favor of static object tree with children

--- src/_2021_07/HanddrawnTree.java
+++ src/_2021_07/HanddrawnTree.java
@@ -12,12 +12,7 @@ public class HanddrawnTree extends KrabApplet {
     private PImage img;
     int windowWidth = 1089;  // 3024 * 0.36;
     int windowHeight = 1452; // 4032 * 0.36;
-    ArrayList<PGraphics> pieces = new ArrayList<>();
-    ArrayList<ArrayList<Character>> treeGenerations = new ArrayList<>();
-    ArrayList<Character> axiom = charsAsList("[F]4-F".toCharArray());
-
-    String ruleString = /* F = */  "|[+F][-F]"; // maybe swap the L, R
-    float baseAngle = radians(45);
+    Branch tree = new Branch(PI);
 
     public static void main(String[] args) {
         KrabApplet.main(java.lang.invoke.MethodHandles.lookup().lookupClass());
@@ -34,120 +29,23 @@ public class HanddrawnTree extends KrabApplet {
         surface.setAlwaysOnTop(true);
     }
 
-    float lastGenerationCount = 0;
 
     public void draw() {
-        pg = updateGraphics(pg, img.width, img.height, P2D);
+        pg = updateGraphics(pg, img.width, img.height, P3D);
         pg.beginDraw();
         pg.imageMode(CORNER);
         pg.image(img, 0, 0);
-        updateTreeDepth();
         reloadPieces();
-        drawTree();
+        translate2D(pg, "center");
+        tree.update();
+        if(button("replant")){
+            tree = new Branch(PI);
+        }
         pg.endDraw();
         imageMode(CORNER);
         image(pg, 0, 0);
         rec(pg, sliderInt("frames", 360));
         gui();
-    }
-
-    private void updateTreeDepth() {
-        int maxGenerations = sliderInt("max generations", 3);
-        if (maxGenerations != lastGenerationCount) {
-            generateAllLSystems(maxGenerations);
-        }
-        lastGenerationCount = maxGenerations;
-    }
-
-    private void generateAllLSystems(int maxGenerations) {
-        ArrayList<Character> nextTree = new ArrayList<>();
-        ArrayList<Character> tree = new ArrayList<>(axiom);
-        ArrayList<Character> rule = charsAsList(ruleString.toCharArray());
-        for (int genIndex = 0; genIndex < maxGenerations; genIndex++) {
-            for (char c : tree) {
-                if (c == 'F') {
-                    nextTree.addAll(rule);
-                } else {
-                    nextTree.add(c);
-                }
-            }
-            tree = new ArrayList<>(nextTree);
-            nextTree.clear();
-            treeGenerations.add(new ArrayList<>(tree));
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (Character character : tree) {
-            sb.append(character);
-        }
-        println(sb.toString());
-    }
-
-    ArrayList<Character> charsAsList(char[] chars) {
-        ArrayList<Character> list = new ArrayList<>();
-        for (char aChar : chars) {
-            list.add(aChar);
-        }
-        return list;
-    }
-
-    // F = draw fixed line and translate
-    // | = draw depth line and translate
-    // [ = push
-    // ] = pop
-    // +-0.23# = rotate right(offset)
-    // -+0.1# = rotate left(offset)
-
-    private void drawTree() {
-        group("tree");
-        baseAngle = radians(slider("base angle", 45));
-        int depth = 0;
-        int maxDepth = 3;
-        int currentGenerationIndex = sliderInt("generation", 1);
-        currentGenerationIndex = constrain(currentGenerationIndex, 0, treeGenerations.size() - 1);
-        ArrayList<Character> tree = treeGenerations.get(currentGenerationIndex);
-        float baseLength = slider("base length", 100);
-        translate2D(pg, "center");
-        pg.rotate(slider("rotate"));
-        pg.imageMode(CENTER);
-        pg.strokeWeight(1.99f);
-        pg.stroke(255);
-//        pg.ellipse(0,0,50,50);
-        for (int i = 0, treeSize = tree.size(); i < treeSize; i++) {
-            Character character = tree.get(i);
-            PGraphics piece = pieces.get(floor(map(depth, 0, maxDepth, 0, pieces.size()-1)));
-            if (character == 'F') {
-                pg.image(piece, 0, 0);
-                pg.translate(0, piece.height * baseLength);
-            } else if (character == '|') {
-                float ratio = 13f / 20;
-                float depthNorm = norm(depth, 0, maxDepth) / ratio;
-//                pg.line(0, 0, 0, baseLength*depthNorm);
-                pg.image(piece, 0, 0, 0, piece.height * depthNorm);
-                pg.translate(0, baseLength*depthNorm);
-            } else if (character == '[') {
-                pg.pushMatrix();
-                depth++;
-                maxDepth = max(depth, maxDepth);
-            } else if (character == ']') {
-                pg.popMatrix();
-                depth--;
-            } else if (character == '+' || character == '-') {
-                int orientation = (character == '+' ? 1 : -1);
-                Character previousChar = tree.get(i - 1);
-                int repeats = 1;
-                try {
-                    repeats = Integer.parseInt(previousChar.toString());
-                } catch (Exception ex) {
-                    // shh
-                }
-                for (int repeat = 0; repeat < repeats; repeat++) {
-                    pg.rotate(baseAngle * orientation);
-                }
-
-            }
-        }
-        resetGroup();
     }
 
     private void reloadPieces() {
@@ -157,11 +55,9 @@ public class HanddrawnTree extends KrabApplet {
         uniform(chromaKeyShader).set("lowBound", slider("low bound"));
         uniform(chromaKeyShader).set("highBound", slider("high bound", 1f));
         if (toggle("update pieces", true)) {
-            pieces.clear();
-            for (int i = 0; i < 7; i++) {
+            for (int i = 0; i < slider("max pieces", 7); i++) {
                 group("" + i);
                 PGraphics piece = getRectangleAsShadedCanvas(img, chromaKeyShader, i, sliderXY("x,y"), sliderXY("w,h", 100));
-                pieces.add(piece);
                 PVector pos = sliderXY("pos", width / 2f, height / 2f);
                 if (toggle("debug")) {
                     pg.imageMode(CENTER);
@@ -170,5 +66,50 @@ public class HanddrawnTree extends KrabApplet {
             }
         }
         resetGroup();
+    }
+
+    class Branch{
+        ArrayList<Branch> children = new ArrayList<>();
+        float angle;
+        int frameCreated = frameCount;
+        int growthPeriod = 120;
+        float lengthOffsetSpread = slider("20");
+        float lengthOffset = random(-lengthOffsetSpread, lengthOffsetSpread);
+        float length = 100 + lengthOffset;
+        float prevGrowthNorm = 0;
+
+        public Branch(float angle) {
+            this.angle = angle;
+        }
+
+        void update(){
+            length = slider("length", 100);
+
+            pg.rotate(angle);
+            draw();
+            pg.pushMatrix();
+            pg.translate(0, length);
+            pg.scale(slider("scaledown",0.8f));
+            for(Branch child : children){
+                child.update();
+            }
+            pg.popMatrix();
+        }
+
+        void draw(){
+            float growthNorm = map(frameCount, frameCreated, frameCreated + growthPeriod, 0, 1);
+            float growthIndex = min(floor(growthNorm), sliderInt("max pieces", 7));
+            growthNorm = min(growthNorm, 1);
+            if(prevGrowthNorm != 1 && growthNorm == 1 && toggle("grow", true)){
+                for (int i = 0; i < random(1, 3); i++) {
+                    children.add(new Branch(randomGaussian() * slider("branch spread", HALF_PI)));
+                }
+            }
+            prevGrowthNorm = growthNorm;
+            pg.strokeWeight(1+growthIndex + growthNorm);
+            pg.stroke(255);
+            pg.line(0, 0, 0, growthNorm * length);
+            pg.fill(255);
+        }
     }
 }
